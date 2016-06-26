024212a1a83aaf358d0a20164d5928db31d52fae
wangyong <wangyongyeah@126.com>
2016-06-26 23:50:24 +0800

    '登录功能'

--- src/main/java/com/controller/UserContoller.java
+++ src/main/java/com/controller/UserContoller.java
@@ -1,5 +1,8 @@
 package com.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,20 @@ public class UserContoller {
     @RequestMapping("/user")
     public String user() {
         return "welcome,user...";
+    }
+
+    @RequestMapping("/login")
+    public String login(String username, String password) {
+
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+
+        try {
+            SecurityUtils.getSubject().login(token);
+        } catch (AuthenticationException e) {
+
+        }
+
+        return null;
     }
 
 
